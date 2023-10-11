2bce0a70b3922ef6129d330c7f3f6098ec6ac29b
Caleb <chorn24@basecampcodingacademy.org>
2023-10-11 13:29:52 -0500

    Added a dark mode

---
+++ main.js
@@ -0,0 +1,21 @@
+function ToggleDark() {
+    var element1 = document.body;
+    element1.classList.toggle("DarkModeBody");
+
+    var element2 = document.getElementById("HeadBod");
+    element2.classList.toggle("DarkModeHeader");
+
+    
+    var element3 = document.getElementById("DarkModeTable");
+    element3.classList.toggle("DarkBackground");
+
+    var element3 = document.getElementById("DarkModeTable1");
+    element3.classList.toggle("DarkBackground");
+
+    var element4 = document.getElementById("DarkMode");
+    element4.classList.toggle("ButtonDark")
+
+    var element4 = document.getElementById("Me");
+    element4.classList.toggle("DarkMe")
+    
+}
