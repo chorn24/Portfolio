0f386cc304368565ebde7b089208f70aedbdff33
chorn24 <chorn24@basecampcodingacademy.org>
2024-01-25 10:25:13 -0600

    Improved Visuals

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ function ToggleDark() {
     element1.classList.toggle("DarkModeBody");
 
     var element2 = document.getElementById("HeadBod");
-    element2.classList.toggle("DarkModeHeader");
-
+    if(element2.classList.contains("Header_"))
+    {
+        element2.classList.remove("Header_")
+        element2.classList.add("DarkModeHeader")
+    }
+    else
+    {
+        element2.classList.remove("DarkModeHeader")
+        element2.classList.add("Header_")
+    }
+    // element2.classList.toggle("DarkModeHeader");
     
     var element3 = document.getElementById("DarkModeTable");
     element3.classList.toggle("DarkBackground");
@@ -23,5 +32,11 @@ function ToggleDark() {
 
     var element4 = document.getElementById("Me");
     element4.classList.toggle("DarkMe")
+
+    var element5 = document.getElementById("Res");
+    element5.classList.toggle("ResinButtonDark")
+
+    var element6 = document.getElementById("In");
+    element6.classList.toggle("ResinButtonDark")
     
 }
